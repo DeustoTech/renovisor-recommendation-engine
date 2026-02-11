54d75fc58777ff5ca7bba750434a48fe5a77d4ea
maria23p <126978708+maria23p@users.noreply.github.com>
2026-02-11 13:44:34 +0100

    cambios

---
+++ Clustering/cobertura.R
@@ -0,0 +1,95 @@
+library(dplyr)
+
+########################## preprocess 
+
+norm <- function(x) {
+  x <- tolower(x)
+  x <- gsub("[[:punct:]]", "", x)
+  x <- gsub("[[:space:]]+", "", x)
+  x
+}
+
+#poner apra todos
+clusters_df <- read.csv("resultsD_plot/D_6/greedy_cluster_centers.csv", stringsAsFactors = FALSE)
+
+cluster_dets <- norm(colnames(clusters_df)[-1])
+cluster_mat <- as.matrix(clusters_df[, -1])
+
+expert_csv_to_binary <- function(expert_csv, determinantes) {
+  expert_df <- read.csv(expert_csv, stringsAsFactors = FALSE, check.names = FALSE)
+  expert_df <- expert_df[, colSums(!is.na(expert_df) & expert_df != "") > 0]  # quitar columnas vacías
+  expert_mat <- t(as.matrix(expert_df))
+  
+  expert_bin <- t(apply(expert_mat, 1, function(row) {
+    sapply(determinantes, function(det) as.integer(det %in% norm(row)))
+  }))
+  
+  rownames(expert_bin) <- rownames(expert_mat)
+  expert_bin
+}
+
+compute_coverage_binary <- function(cluster_mat, expert_bin, cluster_names = NULL, expert_names = NULL, cluster_dets = NULL) {
+  
+  n_clusters <- nrow(cluster_mat)
+  n_experts <- nrow(expert_bin)
+  
+  if (is.null(cluster_names)) cluster_names <- 1:n_clusters
+  if (is.null(expert_names)) expert_names <- rownames(expert_bin)
+  if (is.null(cluster_dets)) cluster_dets <- colnames(cluster_mat)
+  
+  coverage_list <- list()
+  
+  for (i in 1:n_clusters) {
+    cluster_row <- cluster_mat[i, ] > 0
+    vol_cluster <- sum(cluster_row)
+    active_dets <- cluster_dets[cluster_row]       # determinantes activos del cluster
+    cluster_all <- paste(active_dets, collapse = ", ")  # todos los determinantes del cluster
+    
+    for (j in 1:n_experts) {
+      expert_row <- expert_bin[j, ] > 0
+      vol_expert <- sum(expert_row)
+      expert_dets <- cluster_dets[expert_row]       # determinantes activos del experto
+      expert_all <- paste(expert_dets, collapse = ", ") # todos los determinantes del experto
+      
+      common_dets <- intersect(active_dets, expert_dets)
+      vol_common <- length(common_dets)
+      
+      coverage_list[[length(coverage_list)+1]] <- data.frame(
+        cluster = cluster_names[i],
+        expert_row = expert_names[j],
+        vol_cluster = vol_cluster,
+        cluster_dets_all = cluster_all,
+        vol_expert = vol_expert,
+        expert_dets_all = expert_all,
+        vol_common = vol_common,
+        common_dets = paste(common_dets, collapse = ", "),
+        pct_cluster_covered = ifelse(vol_cluster == 0, 0, vol_common / vol_cluster * 100),
+        pct_expert_covered = ifelse(vol_expert == 0, 0, vol_common / vol_expert * 100),
+        stringsAsFactors = FALSE
+      )
+    }
+  }
+  
+  do.call(rbind, coverage_list)
+}
+
+expert_bin <- expert_csv_to_binary("data/archetypeExperts.csv", cluster_dets)
+coverage_experts <- compute_coverage_binary(cluster_mat, expert_bin)
+
+kmeans_bin <- expert_csv_to_binary("data/archetypeKmeans.csv", cluster_dets)
+coverage_kmeans <- compute_coverage_binary(cluster_mat, kmeans_bin)
+
+sins_bin <- expert_csv_to_binary("data/archetypeSINS.csv", cluster_dets)
+coverage_sins <- compute_coverage_binary(cluster_mat, sins_bin)
+
+write.csv(coverage_experts, "resultsD_plot/coverage_experts_D6.csv", row.names = FALSE)
+write.csv(coverage_kmeans, "resultsD_plot/coverage_kmeans_D6.csv", row.names = FALSE)
+write.csv(coverage_sins, "resultsD_plot/coverage_sins_D6.csv", row.names = FALSE)
+
+
+################################################ plot
+library(ggplot2)
+ggplot(coverage_experts, aes(x=cluster, y=pct_cluster_covered, fill=expert_row)) +
+  geom_bar(stat="identity", position="dodge") +
+  labs(title="Cobertura de clusters vs expertos", y="% de cluster cubierto") +
+  theme_minimal()
--- Clustering/compare.R
+++ Clustering/compare.R
@@ -1,15 +1,11 @@
-library(dplyr)
 
+library(clue)  # solve_LSAP --> maximiza global!!!!
 
-centers_df <- read.csv( "resultsD_plot/D_12/greedy_cluster_centers.csv", stringsAsFactors = FALSE)
-
-# quitar columna cluster
+centers_df <- read.csv("resultsD_plot/D_6/greedy_cluster_centers.csv", stringsAsFactors = FALSE)
+centers_mat <- as.matrix(centers_df[, -1])
 determinantes_raw <- colnames(centers_df)[-1]
 
-# matriz de centros
-centers_mat <- as.matrix(centers_df[, -1])
-
-
+# Normalización de texto
 norm <- function(x) {
   x <- tolower(x)
   x <- gsub("[[:punct:]]", "", x)
@@ -20,63 +16,92 @@ norm <- function(x) {
 determinantes <- norm(determinantes_raw)
 colnames(centers_mat) <- determinantes
 
-
-# CSV experto → matriz binaria
 expert_csv_to_binary <- function(expert_csv, determinantes) {
-  expert_df <- read.csv(expert_csv, stringsAsFactors = FALSE)
-
-  # eliminar columnas de indica si existen
-  drop_cols <- c("cluster", "Cluster", "id", "ID", "X")
-  expert_df <- expert_df[, !(names(expert_df) %in% drop_cols), drop = FALSE]
+  expert_df <- read.csv(expert_csv, stringsAsFactors = FALSE, check.names = FALSE)
   
-  expert_bin <- sapply(expert_df, function(col) {
-    vals <- col[!is.na(col) & col != ""]
-    vals <- norm(vals)
-    sapply(determinantes, function(det) {
-      as.integer(det %in% vals)
-    })
-  })
+  # eliminar columnas vacías
+  expert_df <- expert_df[, colSums(!is.na(expert_df) & expert_df != "") > 0]
   
-  # filas = arquetipos expertos
-  expert_bin <- t(expert_bin)
-  return(expert_bin)
+  expert_mat <- t(as.matrix(expert_df))
+  expert_bin <- t(apply(expert_mat, 1, function(row) {
+    sapply(determinantes, function(det) as.integer(det %in% norm(row)))
+  }))
+  rownames(expert_bin) <- rownames(expert_mat)
+  expert_bin
 }
 
-#  Comparar centros con expertos
-compare_centers_with_experts <- function(centers_mat, expert_csv, determinantes) {
+jaccard_similarity <- function(cluster_row, expert_row) {
+  inter <- sum(cluster_row == 1 & expert_row == 1)
+  uni   <- sum(cluster_row == 1 | expert_row == 1)
+  if (uni == 0) return(0)
+  inter / uni * 100
+}
+
+build_similarity_matrix <- function(centers_mat, expert_csv, determinantes) {
   expert_bin <- expert_csv_to_binary(expert_csv, determinantes)
-  results <- data.frame()
-  for (i in seq_len(nrow(centers_mat))) {
-    center <- centers_mat[i, ]
-    center_active <- center > 0
-    if (sum(center_active) == 0) {
-      best_match <- NA
-      pct_match  <- NA
-    } else {
-      similarity <- apply(expert_bin, 1, function(expert_row) {
-        sum(center_active & expert_row == 1) / sum(center_active) * 100
-      })
-      best_match <- rownames(expert_bin)[which.max(similarity)]
-      pct_match  <- max(similarity)
-    }
+  
+  n_clusters <- nrow(centers_mat)
+  n_archetypes  <- nrow(expert_bin)
+  
+  sim_mat <- matrix(0, n_clusters, n_archetypes)
+  rownames(sim_mat) <- centers_df$cluster
+  colnames(sim_mat) <- rownames(expert_bin)
+  
+  for (i in 1:n_clusters) {
+    cluster_row <- centers_mat[i, ] > 0
+    if (sum(cluster_row) == 0) next
     
-    results <- rbind(
-      results,
-      data.frame(
-        cluster    = centers_df$cluster[i],
-        best_match = best_match,
-        pct_match  = pct_match
-      )
-    )
+    for (j in 1:n_archetypes) {
+      sim_mat[i, j] <- jaccard_similarity(cluster_row, expert_bin[j, ])
+    }
   }
   
-  return(results)
+  sim_mat
 }
 
-res_kmeans <- compare_centers_with_experts(centers_mat, "data/archetypeKmeans.csv", determinantes)
-res_sins <- compare_centers_with_experts( centers_mat, "data/archetypeSINS.csv", determinantes)
-res_experts <- compare_centers_with_experts( centers_mat, "data/archetypeExperts.csv", determinantes)
+assign_clusters_exclusive <- function(sim_mat, centers_df, min_pct = 0) {
+  
+  n_clusters <- nrow(sim_mat)
+  n_archetypes  <- ncol(sim_mat)
+  
+  # si hay más clusters que expertos, agregamos columnas ficticias "None"
+  if (n_clusters > n_archetypes) {
+    n_add <- n_clusters - n_archetypes
+    sim_mat <- cbind(sim_mat, matrix(0, n_clusters, n_add))
+    colnames(sim_mat)[(n_archetypes + 1):ncol(sim_mat)] <- paste0("None_", 1:n_add)
+  }
+  
+  assignment <- solve_LSAP(sim_mat, maximum = TRUE)
+  
+  results <- data.frame(
+    cluster    = centers_df$cluster,
+    best_match = NA,
+    pct_match  = NA
+  )
+  
+  for (i in 1:n_clusters) {
+    expert_idx <- assignment[i]
+    expert_name <- colnames(sim_mat)[expert_idx]
+    pct <- sim_mat[i, expert_idx]
+    
+    if (!grepl("^None_", expert_name) && pct >= min_pct) {
+      results$best_match[i] <- expert_name
+      results$pct_match[i]  <- pct
+    }
+  }
+  
+  results
+}
 
-write.csv( res_kmeans, "resultsD_plot/cluster_vs_expertKmeans_D12.csv", row.names = FALSE)
-write.csv( res_sins, "resultsD_plot/cluster_vs_expertSINS_D12.csv", row.names = FALSE)
-write.csv( res_experts, "resultsD_plot/cluster_vs_expertExperts_D12.csv", row.names = FALSE)
+
+sim_kmeans  <- build_similarity_matrix(centers_mat, "data/archetypeKmeans.csv", determinantes)
+sim_sins    <- build_similarity_matrix(centers_mat, "data/archetypeSINS.csv", determinantes)
+sim_experts <- build_similarity_matrix(centers_mat, "data/archetypeExperts.csv", determinantes)
+
+res_kmeans_exclusive  <- assign_clusters_exclusive(sim_kmeans, centers_df, min_pct = 0)
+res_sins_exclusive    <- assign_clusters_exclusive(sim_sins, centers_df, min_pct = 0)
+res_experts_exclusive <- assign_clusters_exclusive(sim_experts, centers_df, min_pct = 0)
+
+write.csv(res_kmeans_exclusive,  "resultsD_plot/cluster_vs_expertKmeans_D6.csv", row.names = FALSE)
+write.csv(res_sins_exclusive,    "resultsD_plot/cluster_vs_expertSINS_D6.csv", row.names = FALSE)
+write.csv(res_experts_exclusive, "resultsD_plot/cluster_vs_expertExperts_D6.csv", row.names = FALSE)
