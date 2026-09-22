f1d5b841a76b016393f9d75bbfcfee23921963c9
Cruz E. Borges <cruz.borges@deusto.es>
2026-09-22 13:06:48 +0200

    mas rutas

--- paper1_cluster/scripts/08_greedy.R
+++ paper1_cluster/scripts/08_greedy.R
@@ -34,14 +34,7 @@ suppressPackageStartupMessages({
 set.seed(123)
 
 # Configuración
-project_root <- path.expand(
-  "~/Desktop/MASTER/recommendation-engine/TFM"
-)
-
-processed_root <- file.path(
-  project_root,
-  "paper1_cluster/data/processed"
-)
+processed_root <- file.path("paper1_cluster/data/processed")
 
 kmeans_dir <- file.path(
   processed_root,
