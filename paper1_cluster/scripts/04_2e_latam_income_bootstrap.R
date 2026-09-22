19c0a5c1865dbaba5e30e728b0b76610cd0ac37c
Cruz E. Borges <cruz.borges@deusto.es>
2026-09-22 12:46:13 +0200

    seguimos corrigiendo rutas

--- paper1_cluster/scripts/04_2e_latam_income_bootstrap.R
+++ paper1_cluster/scripts/04_2e_latam_income_bootstrap.R
@@ -33,14 +33,7 @@ suppressPackageStartupMessages({
 set.seed(123)
 
 # Configuración
-project_root <- path.expand(
-  "~/Desktop/MASTER/recommendation-engine/TFM"
-)
-
-processed_root <- file.path(
-  project_root,
-  "paper1_cluster/data/processed"
-)
+processed_root <- file.path("paper1_cluster/data/processed")
 
 # Dataset individual preparado en 04_2c.
 latam_file <- file.path(
