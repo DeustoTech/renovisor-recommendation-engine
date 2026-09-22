b5df4fe905886e9be4ccaee177568933c1f8b269
Cruz E. Borges <cruz.borges@deusto.es>
2026-09-22 12:36:15 +0200

    arreglamos ruta de fichero

--- paper1_cluster/scripts/04_2b_propensity_bootstrap_eu_pfe_esn_renew_greens_merged_clustering_usable.R
+++ paper1_cluster/scripts/04_2b_propensity_bootstrap_eu_pfe_esn_renew_greens_merged_clustering_usable.R
@@ -17,8 +17,7 @@ suppressPackageStartupMessages({
 set.seed(123)
 
 # Configuración
-project_root <- path.expand("~/Desktop/MASTER/recommendation-engine/TFM")
-processed_root <- file.path(project_root, "paper1_cluster/data/processed")
+processed_root <- file.path("paper1_cluster/data/processed")
 
 in_file <- file.path(
   processed_root,
